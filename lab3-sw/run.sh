32203b41e1f1bbccd68d95ee2081e494d5b0616f
root <ood2103@columbia.edu>
2023-03-22 21:26:34 +0000

    Change to script

--- lab3-sw/run.sh
+++ lab3-sw/run.sh
@@ -1,4 +1,9 @@
 # /bin/bash
-scp tm3147@micro24.ee.columbia.edu:embedded_lab3/lab3-hw/output_files/soc_system.rbf /mnt
-scp tm3147@micro24.ee.columbia.edu:embedded_lab3/lab3-hw/soc_system.dtb /mnt
+
+ifup eth0
+mount /dev/mmcblk0p1 /mnt
+rm /mnt/soc_system.rbf
+rm /mnt/soc_system.dtb
+scp jjl2247@micro24.ee.columbia.edu:Downloads/embedded_lab3/lab3-hw/output_files/soc_system.rbf /mnt
+scp jjl2247@micro24.ee.columbia.edu:Downloads/embedded_lab3/lab3-hw/soc_system.dtb /mnt
 sync
